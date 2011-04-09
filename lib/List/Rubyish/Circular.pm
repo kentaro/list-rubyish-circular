feb7f1e9e0c68204e9e9f6ae9c50e2e7f8400266
Kentaro Kuribayashi <kentarok@gmail.com>
2011-04-09 22:45:50 +0900

    initial import

---
+++ lib/List/Rubyish/Circular.pm
@@ -0,0 +1,39 @@
+package List::Rubyish::Circular;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+List::Rubyish::Circular -
+
+=head1 SYNOPSIS
+
+  use List::Rubyish::Circular;
+
+=head1 DESCRIPTION
+
+List::Rubyish::Circular is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
